9fbda5f0383d8bec9021595036c8a34dd140ed54
Sarah Fuoss <sfuoss@gmail.com>
2021-09-11 09:46:50 -0500

    Added get function from the router to the database.

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -21,6 +21,15 @@ pool.on('error', (error) => {
     console.log('unable to connect to postgresql', error);
 });
 
+router.get('/', (req,res) => {
+    const queryText = 'SELECT * FROM "tasks"';
+    pool.query(queryText).then( (result) => {
+    res.send(results.rows)
+    }).catch(error => {
+        console.log('There was an error getting the tasks', error);
+        res.sendStatus(500);
+    })
+})
 
 
 

55d59abb8f058113133562a456005d4943f04c7a
Sarah Fuoss <sfuoss@gmail.com>
2021-09-12 12:25:11 -0500

    Added delete button and funcitonality.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,7 @@ $(document).ready(onReady);
 function onReady() {
     displayTasks();
     $('#task-table').on('click','.markDone',updateTask);
+    $('#task-table').on('click','.delete-button',deleteTask);
 }
 
 function displayTasks() {
@@ -19,6 +20,7 @@ function displayTasks() {
         <tr>
         <td class="taskListed" "done" id="${tasks.id}">${tasks.taskItem}</td>
         <td><input type="checkbox" class="markDone" data-id="${tasks.id}"></td>
+        <td><button class="delete-button" data-id="${tasks.id}">Delete</button></td>
         </tr>
         `);
         }
@@ -28,35 +30,34 @@ function displayTasks() {
 }
 
 
-
 // Change status of task to true
 function updateTask() {
     const taskId = $(this).data('id');
+    // add strikethrough to task text when checkbox clicked
     $(this).parent().parent().toggleClass('done');
     console.log('button was clicked');
     $.ajax({
         method: 'PUT',
         url: `/tasks/${taskId}`
     }).then(function(response) {
-        console.log(response);
-        
         console.log('Item was updated');
     }).catch(function(error){
         console.log('unable to update');
     })
 }
 
-// // Delete task from table
-// function deleteTask() {
-//     console.log('button was clicked');
-//     $.ajax({
-//         method: 'DELETE',
-//         url: `/tasks/${taskId}`
-//     }).then(function(response) {
-//         console.log(response);
-//         console.log('Item was deleted');
-//     }).catch(function(error){
-//         console.log('unable to update');
-//     })
-//     displayTasks();
-//}
+// Delete task from table
+function deleteTask() {
+    const taskId = $(this).data('id');
+    console.log('button was clicked');
+    $.ajax({
+        method: 'DELETE',
+        url: `/tasks/${taskId}`
+    }).then(function(response) {
+        console.log(response);
+        console.log('Item was deleted');
+    }).catch(function(error){
+        console.log('unable to update');
+    })
+    displayTasks();
+}
